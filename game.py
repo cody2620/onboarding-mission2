286b176fec07d7e23f7346809b6361ffbf11b3f8
김수민 <aever1443475@c6r2s1.codyssey.kr>
2026-08-02 18:25:09 +0900

    feat: 퀴즈 메뉴 구현

---
+++ game.py
@@ -0,0 +1,12 @@
+class QuizGame:
+    def start_quiz(self):
+        print("퀴즈를 시작합니다.")
+
+    def add_quiz(self):
+        print("새로운 퀴즈 등록")
+
+    def show_quiz(self):
+        print("등록된 퀴즈 조회")
+
+    def show_myscore(self):
+        print("현재까지의 내 점수")
--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+from game import QuizGame
+
 print("Hello Quiz")
+
+def main():
+    game = QuizGame()
+
+    while True:
+        print("제목")
+        print("퀴즈 출제")
+        print("퀴즈 등록")
+        print("퀴즈 목록")
+        print("점수 확인")
+        print("종료")
+
+        choice = input("메뉴를 선택하세요: ")
+
+        if choice == '1':
+            game.start_quiz()
+
+        elif choice == '2':
+            game.add_quiz()
+
+        elif choice == '3':
+            game.show_quiz()
+
+        elif choice == '4':
+            game.show_myscore()
+
+        elif choice == '5':
+            print("\n프로그램을 종료합니다.")
+            break
+        else:
+            print("\n 잘못된 입력입니다.")
+
+if __name__ == "__main__": 
+    main()
